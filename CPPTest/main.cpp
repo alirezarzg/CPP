0e51cd8fd583606ac2d312bcebbfd8cabd9c8440
Alireza Rezagholian <alireza.rzg@gmail.com>
2019-01-08 06:17:30 +0330

    initial commit

--- CPPTest/main.cpp
+++ CPPTest/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#define Pi 3.14
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std ;
+
+int main() {
+    int a , b , c , d ;
+    float e1 , e2 ;
+    string str ;
+    
+    cin >> a >> b ; // without using the STD namesapce -> std :: cin >> a >> b ; *** also b = cin.get() ; == cin >> b ;
+    c = a++ * b++ ;
+    d = a++ * ++b ;
+    cout << "c: " << c << endl << "d: " << d << "\n" /* equals endl */ ;
+    
+    //use of #define
+    cout << "a * Pi: " << a * Pi << endl ;
+    
+    // the way we cast our variables type in c++
+    e1 = (a + b + c) / d ;
+    cout << "e1 = (a + b + c) / d = " << e1 << endl ;
+    e2 = static_cast<float>(a + b + c) / d ;
+    cout << "e2 = static_cast<float>(a + b + c) / d = " << e2 << endl ;
+    
+    //use string
+    cout <<  "enter a name: " << endl ;
+    cin >> str ;
+    cout << "you entered: " << str << endl ;
+    
+    /*    cin.ignore() ; // could be replaced with -> cin.get() ;
+     cin.get() ; // (in WindowsOS)used to keep the terminal window still open after program ending . */
+    return 0 ;
 }
