54711251e8f95016099539af16e4e5eeecc5a9c7
Alireza Rezagholian <alireza.rzg@gmail.com>
2019-01-08 06:16:02 +0330

    Initial Commit

---
+++ CPPTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPTest
+//
+//  Created by Alireza on 10/18/1397 AP.
+//  Copyright © 1397 Alireza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
